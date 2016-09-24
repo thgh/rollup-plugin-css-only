5415a12e9564e810b27e4ebdebf49419184f848f
Thomas Ghysels <info@thomasg.be>
2016-09-24 23:08:17 +0200

    Fix console output

--- src/index.js
+++ src/index.js
@@ -68,3 +68,19 @@ export default function css (options = {}) {
     }
   }
 }
+
+export function emitted (text, bytes) {
+  console.log(green(text), getSize(bytes))
+}
+
+export function green (text) {
+  return '\u001b[1m\u001b[32m' + text + '\u001b[39m\u001b[22m'
+}
+
+export function getSize (bytes) {
+  return bytes < 10000
+    ? bytes.toFixed(0) + ' B'
+    : bytes < 1024000
+    ? (bytes / 1024).toPrecision(3) + ' kB'
+    : (bytes / 1024 / 1024).toPrecision(4) + ' MB'
+}
